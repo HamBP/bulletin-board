841bca48c9803eddbcf979f0d6b15395ecdbb826
algosketch <sjy_9484@naver.com>
2021-01-14 05:51:42 +0900

    글 작성시 날짜 표기

--- bulletin-board/routes/create.js
+++ bulletin-board/routes/create.js
@@ -18,8 +18,13 @@ router.post('/', function(req, res, next) {
     console.log('create : an article written.');
     var body = req.body;
 
+    // month | day < 10 일 때 앞에 0 붙이기
+    var month = new Date().getMonth() + 1 < 10 ? '0' + (new Date().getMonth() + 1) : new Date().getMonth();
+    var day = new Date().getDate() < 10 ? '0' + new Date().getDate() : new Date().getDate();
+    var date = month + '-' + day;
+
     client.query('insert into articles (title, nickname, date, content) values (?, ?, ?, ?)', 
-    [body.title, '익명', '0101', body.content], function () {
+    [body.title, '익명', date, body.content], function () {
         res.redirect('/');
     });
 });
