5d649b1fb2450e99ee3a6a1db0e53a569762266c
algosketch <sjy_9484@naver.com>
2021-01-13 23:08:26 +0900

    글 목록 보이기

--- bulletin-board/routes/index.js
+++ bulletin-board/routes/index.js
@@ -15,7 +15,7 @@ router.get('/', function(req, res, next) {
   client.query('select * from articles', function(err, result, fields) {
     console.log(result[1]);
 
-    res.render('index', { title: '게시판.', div: '<h1>123</h1>' });
+    res.render('index', { title: '게시판.', articles: result });
   });
   //res.render('index', { title: '게시판' });
 });
